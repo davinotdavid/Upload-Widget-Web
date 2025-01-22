6e22580f6d1f6ede8863a2f957d86c82f9fb58a8
Davi Nakano <davinakanoca@gmail.com>
2025-01-22 17:56:54 -0600

    feat: Add axios and make upload and cancel functions work

---
+++ src/http/upload-file-to-storage.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+interface UploadFileToStorageParams {
+  file: File
+}
+
+interface UploadFileToStorageOptions {
+  signal?: AbortSignal
+}
+
+export async function uploadFileToStorage(
+  { file }: UploadFileToStorageParams,
+  options?: UploadFileToStorageOptions
+) {
+  const data = new FormData()
+  data.append('file', file)
+
+  const response = await axios.post<{url: string }>('http://localhost:3333/uploads', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+    signal: options?.signal
+  })
+
+  return { url: response.data.url }
+}
